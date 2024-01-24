2fb8605b2ba0f562da36043246e4e7a1e5893b07
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-01-24 12:48:11 +0530

    2nd commit on 24th Jan

--- script.js
+++ script.js
@@ -32,8 +32,23 @@ async function searchImages(){
 
         imageWrapper.appendChild(image)
         imageWrapper.appendChild(imageLink)
-        imageWrapper.appendChild(imageWrapper)
+        searchResults.appendChild(imageWrapper)
 
 
-    })
+    });
+    page++;
+    if (page > 1){
+        showMore.style.display="block"
+    }
 }
+
+formEl.addEventListener("submit",(event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages()
+})
+
+showMore.addEventListener("click",() => {
+    
+    searchImages()
+})
